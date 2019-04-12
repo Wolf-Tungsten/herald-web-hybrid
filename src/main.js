2e8eced369134d96e7adbdcb6a28caed53f8bbf2
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-04-12 20:40:58 +0800

    升级讲座页

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import Persist from 'vue-component-persist'
 //import { Main } from '../node_modules/element-ui';
 
 // 版本信息
-window.versionCode = 19
+window.versionCode = 20
 
 // 定时检查token情况
 setInterval (()=>{
